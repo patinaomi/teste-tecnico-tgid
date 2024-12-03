5741106d299acc0c2f99454c14a2ee3f7bd3e11e
Patricia Naomi <patricianaomiyama@gmail.com>
2024-12-03 20:21:17 -0300

    FIX: Validação nas dtos

--- tgid/src/main/java/br/com/tgid/dtos/request/ClienteRequest.java
+++ tgid/src/main/java/br/com/tgid/dtos/request/ClienteRequest.java
@@ -2,24 +2,24 @@ package br.com.tgid.dtos.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
 public record ClienteRequest(
-        @NotBlank
+        @NotBlank(message = "O nome do Cliente não pode ser nulo.")
         String nome,
 
-        @NotBlank
+        @NotBlank(message = "O sobrenome do Cliente não pode ser nulo.")
         String sobrenome,
 
-        @NotBlank
-        @CPF  // para fazer a validação do CPF
+        @NotBlank(message = "O CPF do Cliente não pode ser nulo.")
+        @CPF(message = "CPF do cliente inválido")  // para fazer a validação do CPF
         String cpf,
 
-        @Email
-        @NotBlank
+        @Email(message = "Email inválido")
+        @NotBlank(message = "O email do Cliente não pode ser nulo.")
         String email,
 
-        @NotBlank
         Double saldo) {
 
 
--- tgid/src/main/java/br/com/tgid/dtos/request/EmpresaRequest.java
+++ tgid/src/main/java/br/com/tgid/dtos/request/EmpresaRequest.java
@@ -1,16 +1,26 @@
 package br.com.tgid.dtos.request;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.hibernate.validator.constraints.br.CNPJ;
 
 public record EmpresaRequest(
+
+        @NotBlank(message = "O nome da Empresa não pode ser nulo.")
         String nome,
 
-        @CNPJ
+        @CNPJ(message = "CNPJ inválido")
+        @NotBlank(message = "O CNPJ da Empresa não pode ser nulo.")
         String cnpj,
 
-        @Email
+        @Email(message = "Email inválido")
+        @NotBlank(message = "O email da Empresa não pode ser nulo.")
         String email,
+
+        @NotNull(message = "O saldo da Empresa é obrigatório.")
+        @Positive(message = "O saldo da Empresa deve ser positivo.")
         Double saldo
 )
 {
